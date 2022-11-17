198e7da2879a8fc2c917b5c542bb26d9fe36ee1c
Leonardo Fujimura <99231242+KinoDesu@users.noreply.github.com>
2022-11-16 22:08:52 -0300

    att js

---
+++ epictask/assets/js/index.js
@@ -0,0 +1,23 @@
+let pontos = 0
+
+const botao = document.querySelector("#botaoAdicionar")
+
+botao.addEventListener("click", (e) => {
+    e.preventDefault()
+    let titulo = document.querySelector("#titulo").value
+    let pontos = Number(document.querySelector("#pontos").value)
+    let categoria = document.querySelector("#categoria").value
+
+    const tarefa = {
+        titulo,
+        pontos,
+        categoria
+    }
+
+    const card = Card(tarefa)
+
+    document.querySelector("#tarefas").appendChild(card)
+
+    console.log(titulo)
+    console.log(pontos)
+})
